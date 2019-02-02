83985da8dcd51afb10838940e231ac0aa5bca615
Alexander Maisch <alexander.maisch@web.de>
2019-02-02 13:57:51 +0100

    completed implementation of SkillObject

--- src/internal/SkillObject.py
+++ src/internal/SkillObject.py
@@ -16,6 +16,24 @@ class SkillObject(abc.ABC):
     def isDeleted(self):
         return 0 == self.skillID
 
+    def set(self, field, value):
+        field.set(self, value)
+
+    def get(self, field):
+        return field.get(self)
+
+    def prettyString(self, sf):
+        string = "Age(self: " + self  # TODO
+        p = sf.poolByName.get(self.skillName())
+        self.printFs(p.allFields(), string)
+        string += ")"
+        return string
+
+    def printFs(self, fieldIterator, string):
+        while fieldIterator.hasNext():
+            f = fieldIterator.__next__()
+            string += ", " + f.name + ": " + f.get(self)
+
 
 class SubType(SkillObject, NamedType):
 
